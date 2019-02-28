a5374b31c81f8b5470240321af6f5b10860e0f7e
theTisrock <torok.chris@gmail.com>
2019-02-27 19:53:48 -0500

    "Added models"

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,13 @@
 
 from flask import Flask
 from config import Config  # see Config class
+from flask_sqlalchemy import SQLAlchemy  # Object Relational Mapper
+from flask_migrate import Migrate  # Alembic migration extension for flask
 
 app = Flask(__name__)
-app.config.from_object(Config)  # readin the configuration
+app.config.from_object(Config)  # read-in the configuration
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)  # makes sense. Migration links app code & DB.
+# Similar to Code First versioning in ASP.NET Entity Framework
 
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -5,7 +5,14 @@
 # Flask Mega Tutorial by Miguel Grinberg. Section 3, lecture 11
 
 import os
+base_directory = os.path.abspath(os.path.dirname(__file__))  # generate an abspath to dir containing db file
+
 
 class Config(object):
     # to keep this secret, keep in an environment variable
     SECRET_KEY = os.environ.get('SECRET_KEY') or "you-will-never-guess"
+    # tell sqlalchemy the location of my SQL database
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              "sqlite:///" + os.path.join(base_directory, "app.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # used to signal application whenever an object changes.
+    # If not set to false, will alert you every time you start the server.
