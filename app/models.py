80fe7c5cf20d92d25bd466a0d9f153fba0afd2a9
Chris Torok <torok.chris@gmail.com>
2019-03-02 06:12:34 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):  # db.Model is the base class for SQLAlchemy
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')  # apparently, this line places
-    # an 'author' column in the Post table that will this table
+    # an 'author' column in the Post table that will reference this table
 
     def __repr__(self):
         #  repr == representation. provides a representation of the model
