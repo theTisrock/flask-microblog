972bcd1e6b0d6b08d9199d0a0473a46f318db303
theTisrock <torok.chris@gmail.com>
2019-03-03 02:14:39 -0500

    testing db

--- microblog.py
+++ microblog.py
@@ -1,1 +1,9 @@
 from app import app  # this is where flask obtains the application instance
+
+# customize the shell context to include common imports
+from app import db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db, 'User': User, 'Post': Post}
