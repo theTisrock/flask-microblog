cbb403c6a7d919ca3bcd0c031e8b7fca588f8987
Christopher Torok <torok.chris@gmail.com>
2019-05-20 22:32:38 -0400

    "Getting RequestError from Elasticsearch. May be a versioning issue. Failed gracefully w/ try-except-finally block."

--- microblog_app/main/routes.py
+++ microblog_app/main/routes.py
@@ -8,6 +8,7 @@ from flask_login import current_user, login_required
 from flask_babel import get_locale
 from guess_language import guess_language
 from flask_babel import _
+from elasticsearch.exceptions import RequestError
 # my stuff
 from microblog_app import db
 from microblog_app.translate import translate
@@ -32,11 +33,11 @@ def load_context():
         'register': Action.register,
         'request_password_reset': Action.request_password_reset
     }
-    search_form = SearchForm()
+    g.search_form = SearchForm()
     if current_user.is_anonymous:
-        search_form = None
+        g.search_form = None
     return dict(
-        Action=action, search_form=search_form
+        Action=action, search_form=g.search_form
     )
 
 
@@ -44,13 +45,31 @@ def load_context():
 def before_request():
     if current_user.is_authenticated:
         current_user.timestamp_on_request()
+        g.search_form = SearchForm()
     g.locale = str(get_locale())  # g is a static field that can hold data and is accessible between request points
 
 
 @bp.route("/search", methods=['GET'])
+@login_required
 def search():
-    # how do I vasearch_form.q.label.textlidate the form without using g and is it too much work???
-    return "yay"
+    # how do I access the form without using g and is it too much work???
+    try:
+        if not g.search_form.validate():
+            return redirect(url_for(Action.explore))
+        page = request.args.get('page', 1, type=int)
+        posts, total = Post.search(g.search_form.q.data, page,
+                                   current_app.config['POSTS_PER_PAGE'])
+        next_url = url_for(Action.search, q=g.search_form.q.data, page=page + 1) \
+            if total > page * current_app.config['POSTS_PER_PAGE'] else None
+        prev_url = url_for(Action.search, q=g.search_form.q.data, page=page - 1) \
+            if page > 1 else None
+        return render_template('search.html', title=_('Search'), posts=posts,
+                               next_url=next_url, prev_url=prev_url)
+    except RequestError:
+        flash("The search feature is not working currently.")
+
+    finally:
+        return redirect(url_for(Action.explore))
 
 
 @bp.route(URLRoute.edit_profile, methods=['GET', 'POST'])  # edit profile
